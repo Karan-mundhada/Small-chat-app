ec20b82a25932882d9af1d11f1f41b523a631c31
Karan-mundhada <92524719+Karan-mundhada@users.noreply.github.com>
2024-01-21 04:36:59 +0530

    finished app with pretty frontend

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh" }}>
+      <PrettyChatWindow
+        projectId="0f1ba0c8-d151-4ed6-99fd-9ce442bc71c8"
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
